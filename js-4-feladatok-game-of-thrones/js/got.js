6c11c644ffcd686116d3306d9105ee76fb71ad94
BiroKrisz <biro.s.kr@gmail.com>
2021-11-23 12:00:47 +0100

    Filepaths...

--- js-4-feladatok-game-of-thrones/js/got.js
+++ js-4-feladatok-game-of-thrones/js/got.js
@@ -3,7 +3,7 @@ const charBio = document.querySelector('.bio__details');
 const searchForm = document.querySelector('.bio__form');
 const inputName = document.querySelector('input');
 
-sortCharacters('json/got.json')
+sortCharacters('./json/got.json')
 
 async function sortCharacters(url) {
     const response = await fetch(url);
@@ -43,14 +43,14 @@ function createArticle(char) {
     <div class="bio__details-img">
     ${(char.picture) ?
                 `<img src="${char.picture}" alt="${char.name}">` :
-                `<img src="assets/placeholder.svg">`
+                `<img src="./assets/placeholder.svg">`
             }
     </bio__details-img>
     <div class="bio__details-title">
         <span>${char.name}</span>
         ${(char.house || char.organization) ?
-                `<img src="assets/houses/${char.house ? char.house : char.organization}.png" alt="${char.name}">` :
-                `<img src="assets/placeholder.svg">`
+                `<img src="./assets/houses/${char.house ? char.house : char.organization}.png" alt="${char.name}">` :
+                `<img src="./assets/placeholder.svg">`
             }
     </div>
     <div class="bio__details-text">${char.bio}</div>
@@ -64,7 +64,7 @@ searchForm.addEventListener('submit', (event) => {
     event.preventDefault();
     const searchedName = inputName.value;
 
-    fetch('json/got.json')
+    fetch('./json/got.json')
         .then(response => response.json())
         .then(allChar => {
             const searchArray = allChar.filter(char => char.name.toLocaleUpperCase() === searchedName.toLocaleUpperCase());
